7b0f1bd8f2e2b038957e02e1fb423a5975f7f632
Aman Arya <125aryaaman@gmail.com>
2024-04-07 23:18:13 +0530

    Added a test api route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
 export async function GET(request: Request) {
-  console.log('GET ', request.url);
-  return Response.json({ message: 'Hello World' });
+  console.log(request.method, request.url);
+  return Response.json({ message: 'API is up and running !' });
 }
