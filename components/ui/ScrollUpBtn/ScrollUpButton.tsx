5d95c0dbbf83f842d2a16b1fdcbb0aedfad98cb2
Aman Arya <125aryaaman@gmail.com>
2024-04-08 00:57:31 +0530

    Fix import statement in ScrollUpButton.tsx

--- components/ui/ScrollUpBtn/ScrollUpButton.tsx
+++ components/ui/ScrollUpBtn/ScrollUpButton.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './ScrollUpButton.module.css';
 
 const ScrollUpButton = ({ element }: { element?: HTMLElement }) => {
