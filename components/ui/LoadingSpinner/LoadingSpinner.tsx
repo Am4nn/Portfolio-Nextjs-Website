d6fc72b4a640373b4760f594f0c02d2c0ff3829a
Aman Arya <125aryaaman@gmail.com>
2024-04-07 23:21:41 +0530

    Refactored LoadingSpinner component

--- components/ui/LoadingSpinner/LoadingSpinner.tsx
+++ components/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import classes from './LoadingSpinner.module.css';
 
 const LoadingSpinner = () => {
-    return <div className={classes.spinner}></div>;
+  return <div className={classes.spinner} />;
 }
 
 export default LoadingSpinner;
