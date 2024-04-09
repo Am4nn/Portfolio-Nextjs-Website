6d3ddfa49ea5f42d52e0110092fa5694ac058abd
Aman Arya <125aryaaman@gmail.com>
2024-04-10 04:20:28 +0530

    Update About module CSS and About component to add responsive styles and introduce new content

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -1,11 +1,66 @@
 import SectionHeading from '@/components/ui/SectionHeading/SectionHeading';
+import Image from 'next/image';
+import StyledLink from '@/components/ui/StyledLink/StyledLink';
+import Skills from './Skills/Skills';
+import styles from './About.module.css';
 
 const About = () => (
   <section id="about">
+
     <SectionHeading
       subText="Introduction"
       headText="About Me"
     />
+
+    <div className={styles.inner}>
+
+      <div className={styles.about_text}>
+        <div>
+          <p>
+            I’m Aman Arya, currently I live in Bengaluru India,
+            working as a Junior Engineer at <StyledLink href='https://www.goldmansachs.com/'>Goldman Sachs</StyledLink>.
+          </p>
+
+          <p>
+            I’ve been flexible with technologies, enjoying the journey from creating games like <StyledLink href='https://github.com/Am4nn/Super-Mario-SFML'>Super Mario</StyledLink> in C++ with SFML,
+            to exploring Java with its <StyledLink href='https://github.com/Am4nn/Auction-App-Using-RMI'>RMI</StyledLink>, <StyledLink href='https://github.com/Am4nn/MultiClient-Java-Server'>Socket</StyledLink> and <StyledLink href='https://github.com/Am4nn/Draw-Graphs'>Swing</StyledLink> libraries.
+            Transitioning to web development, I’ve delved into various libraries and frameworks such as Next.js, TypeScript, Framer Motion, Socket.io and Redux.
+            I also explored <StyledLink href='https://github.com/Am4nn/Online-Judge-Project'>Docker, AWS, DNS management</StyledLink>, and other technologies during this transition.
+          </p>
+
+          <p>
+            During my time at Goldman Sachs, I focused on Tableau, data modeling, and am currently involved in
+            Playwright test automation, Cucumber, and contributing to API services with Spring Boot and RxJava.
+          </p>
+
+          <p>
+            In my free time, I enjoy watching web series, playing video games, and experimenting with new technologies.
+            I’m always eager to learn about new projects, so <StyledLink href='#contact'>please feel free to reach out to me</StyledLink>.
+          </p>
+
+          <p>Here are a few technologies I’ve worked on:</p>
+
+          <Skills />
+
+        </div>
+
+      </div>
+
+      <div className={styles.image_div_wrapper}>
+        <div className={styles.wrapper}>
+          <Image
+            className={styles.img}
+            src="/images/my-pic.png"
+            width={728}
+            height={1125}
+            quality={95}
+            alt="Headshot"
+          />
+        </div>
+      </div>
+
+    </div>
+
   </section>
 );
 
