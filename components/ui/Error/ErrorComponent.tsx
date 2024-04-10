ab7a0c5f19169086ef1ebc024c1342ff8c31c0cd
Aman Arya <125aryaaman@gmail.com>
2024-04-10 14:31:46 +0530

    Update error message display and use next/fonts for Error-Component

--- components/ui/Error/ErrorComponent.tsx
+++ components/ui/Error/ErrorComponent.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import classes from './styles.module.css'
-import Ghost from './Ghost'
-import Link from 'next/link'
-import SlidingButton from '@/components/ui/SlidingButton/SlidingButton'
-import { statusCodes } from '@/utils/status-codes'
+import Link from 'next/link';
+import SlidingButton from '@/components/ui/SlidingButton/SlidingButton';
+import { statusCodes } from '@/utils/status-codes';
+import { montserrat } from '@/utils/fonts';
+import { cn } from '@/utils/cn';
+import Ghost from './Ghost';
+import classes from './styles.module.css';
 
 const ErrorComponent = ({ code }: { code: string }) => {
   const message = statusCodes[code];
   return (
-    <section className={classes.section}>
+    <section className={cn(montserrat.className, classes.section)}>
       <main className={classes.section}>
         <h1 className={classes.h1}>
           {code[0]}
@@ -17,7 +18,7 @@ const ErrorComponent = ({ code }: { code: string }) => {
           </span>
           {code[2]}
         </h1>
-        <h2 className={classes.h2}>Error <span>{message}</span> : {message}</h2>
+        <h2 className={classes.h2}>Error <span>{code}</span> : {message}</h2>
         <p className={classes.p}>Sorry, the page you’re looking for cannot be accessed</p>
 
         <div className={classes.containerx}>
