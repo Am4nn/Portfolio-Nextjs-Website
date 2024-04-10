fc7c879a7e4443a3fd752bb6dbff41d807692177
Aman Arya <125aryaaman@gmail.com>
2024-04-10 15:21:06 +0530

    Refactor StyledLink component to include additional props of Anchor Tag

--- components/ui/StyledLink/StyledLink.tsx
+++ components/ui/StyledLink/StyledLink.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import styles from './StyledLink.module.css';
 
-type StyledLinkProps = {
+interface StyledLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
   children: React.ReactNode;
-};
+}
 
-const StyledLink = ({ href, children }: StyledLinkProps) => (
-  <Link className={styles.link} href={href}>
+const StyledLink = ({ href, children, ...rest }: StyledLinkProps) => (
+  <Link className={styles.link} href={href} {...rest}>
     {children}
   </Link>
 );
