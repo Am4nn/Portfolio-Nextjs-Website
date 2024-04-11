b21fc7e4c5a4d475a552d8e141d1d16b6c65e164
Aman Arya <125aryaaman@gmail.com>
2024-04-11 18:35:36 +0530

    Refactor GridBackgroudLayout component to use arrow function syntax

--- components/ui/GridBackgroudLayout/GridBackgroudLayout.tsx
+++ components/ui/GridBackgroudLayout/GridBackgroudLayout.tsx
@@ -2,14 +2,10 @@ import React from "react";
 
 const GridBackgroudLayout = ({ children }: Readonly<{
   children?: React.ReactNode;
-}>) => {
-  return (
-    <div className="w-full h-auto bg-background text-foreground dark:bg-grid-white/[0.03] bg-grid-black/[0.03]">
-      {/* Radial gradient for the container to give a faded look */}
-      {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
-      {children}
-    </div>
-  );
-}
+}>) => (
+  <div className="w-full h-auto bg-background text-foreground dark:bg-grid-white/[0.03] bg-grid-black/[0.03]">
+    {children}
+  </div>
+);
 
 export default GridBackgroudLayout;
