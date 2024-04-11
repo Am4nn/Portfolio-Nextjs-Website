9fedc685d67e0fb39d4aa081612d7a0cc1473674
Aman Arya <125aryaaman@gmail.com>
2024-04-12 04:03:48 +0530

    Refactor three.js to use TypeScript

--- utils/three.ts
+++ utils/three.ts
@@ -1,4 +1,4 @@
-import { Cache, TextureLoader } from 'three';
+import { Cache, TextureLoader, Scene, Material, Renderer, Object3D, Light, DirectionalLight, AmbientLight } from 'three';
 import { DRACOLoader, GLTFLoader } from 'three-stdlib';
 
 // Enable caching for all loaders
@@ -18,15 +18,20 @@ export const textureLoader = new TextureLoader();
 /**
  * Clean up a scene's materials and geometry
  */
-export const cleanScene = scene => {
-  scene?.traverse(object => {
+export const cleanScene = (scene: Scene | undefined) => {
+  scene?.traverse((object: Object3D) => {
+    //@ts-ignore
     if (!object.isMesh) return;
 
+    //@ts-ignore
     object.geometry.dispose();
 
+    //@ts-ignore
     if (object.material.isMaterial) {
+      //@ts-ignore
       cleanMaterial(object.material);
     } else {
+      //@ts-ignore
       for (const material of object.material) {
         cleanMaterial(material);
       }
@@ -37,16 +42,18 @@ export const cleanScene = scene => {
 /**
  * Clean up and dispose of a material
  */
-export const cleanMaterial = material => {
+export const cleanMaterial = (material: Material) => {
   material.dispose();
 
   for (const key of Object.keys(material)) {
+    //@ts-ignore
     const value = material[key];
     if (value && typeof value === 'object' && 'minFilter' in value) {
-      value.dispose();
+      //@ts-ignore
+      (value as TextureLoader).dispose();
 
       // Close GLTF bitmap textures
-      value.source?.data?.close?.();
+      (value as any).source?.data?.close?.();
     }
   }
 };
@@ -54,27 +61,27 @@ export const cleanMaterial = material => {
 /**
  * Clean up and dispose of a renderer
  */
-export const cleanRenderer = renderer => {
+export const cleanRenderer = (renderer: Renderer | undefined) => {
+  //@ts-ignore
   renderer.dispose();
-  renderer = null;
 };
 
 /**
  * Clean up lights by removing them from their parent
  */
-export const removeLights = lights => {
-  for (const light of lights) {
-    light.parent.remove(light);
+export const removeLights = (lights: (DirectionalLight | AmbientLight)[] | undefined) => {
+  for (const light of lights ?? []) {
+    light.parent?.remove(light);
   }
 };
 
 /**
  * Get child by name
  */
-export const getChild = (name, object) => {
-  let node;
+export const getChild = (name: string, object: Object3D) => {
+  let node: Object3D | undefined;
 
-  object.traverse(child => {
+  object.traverse((child: Object3D) => {
     if (child.name === name) {
       node = child;
     }
