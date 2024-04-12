1d5170d8f616ba83119a5be71ff22ec5f41e4418
Aman Arya <125aryaaman@gmail.com>
2024-04-13 00:48:47 +0530

    Refactor component imports and add ErrorBoundary and WebGLWrapper to DisplacementSphere

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 import React, { Fragment } from "react";
-import GridBackgroudLayout from "@/components/ui/GridBackgroudLayout/GridBackgroudLayout";
-import { DisplacementSphere } from "@/components/ui/DisplacementSphere/DisplacementSphere";
-import MainComponent from "@/components/ui/MainComponent/MainComponent";
+// import dynamic from 'next/dynamic';
+import GridBackgroudLayout from "@/components/wrapper/GridBackgroudLayout/GridBackgroudLayout";
+import MainComponent from "@/components/wrapper/MainComponent/MainComponent";
+import ErrorBoundary from "@/components/wrapper/ErrorBoundary/ErrorBoundary";
+import WebGLWrapper from "@/components/wrapper/WebGLWrapper/WebGLWrapper";
 import { SocialSideBar } from "@/components/ui/LeftSideBar/LeftSideBar";
 import ScrollIndicator from "@/components/ui/ScrollIndicator/ScrollIndicator";
+import DisplacementSphere from "@/components/ui/DisplacementSphere/DisplacementSphere";
 import Footer from "@/components/ui/Footer/Footer";
 import Intro from "./components/Intro/Intro";
 import About from "./components/About/About";
 
+// Only render the DisplacementSphere component on the client side
+// const DisplacementSphere = dynamic(() => import("@/components/ui/DisplacementSphere/DisplacementSphere"), { ssr: false });
+
 export default function Home() {
   return (
     <Fragment>
-      <DisplacementSphere />
+      <ErrorBoundary>
+        <WebGLWrapper>
+          <DisplacementSphere />
+        </WebGLWrapper>
+      </ErrorBoundary>
       <SocialSideBar />
       <GridBackgroudLayout>
         <MainComponent>
