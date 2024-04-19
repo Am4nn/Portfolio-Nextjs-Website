992d925f64cf8d882ce0e877f7e8171ea9a344d8
Aman Arya <125aryaaman@gmail.com>
2024-04-20 02:48:42 +0530

    Added NavIcons component

---
+++ components/ui/NavIcons/NavIcons.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useMediaQuery } from '@mui/material';
+import { Icon } from '@/components/ui/Icons';
+import { socialMediaDetails } from '@/utils/config';
+import { cn } from '@/utils/cn';
+import { useDelayedMount } from '@/hooks';
+import styles from './NavIcons.module.css';
+
+// in milliseconds
+const socialSideBarMountDelay = 500 + 1000;
+
+const fadeInAnimationVariants = {
+  initial: { y: 20, opacity: 0, scale: 0.6, filter: "blur(20px)" },
+  animate: (index: number) => ({
+    y: 0, opacity: 1, scale: 1, filter: "blur(0px)",
+    transition: { duration: 0.3, delay: index * 0.1 }
+  })
+}
+
+export default function SocialNavIcons() {
+  const isMounted = useDelayedMount(socialSideBarMountDelay);
+  const isMobile = useMediaQuery('(max-width: 767.5px)');
+
+  return (
+    <AnimatePresence>
+      {(!isMobile && isMounted) ? <NavIcons /> : null}
+    </AnimatePresence>
+  );
+}
+
+const NavIcons = () => (
+  <div className={cn(styles.StyledSideElement)}>
+    <ul className={styles.StyledSocialList}>
+      {socialMediaDetails && socialMediaDetails.map(({ url, name }, index) => (
+        <motion.li
+          key={index}
+          variants={fadeInAnimationVariants}
+          initial="initial"
+          animate="animate"
+          custom={index}
+        >
+          {url ?
+            <Link href={url} aria-label={name} target="_blank" rel="noreferrer">
+              <Icon name={name} />
+            </Link> :
+            <div className={styles.bar} />
+          }
+        </motion.li>
+      ))}
+    </ul>
+  </div>
+)
