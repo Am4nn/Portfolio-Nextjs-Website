36c52a4f8cc73153a5bcc633ddfcfb37927b7137
Aman Arya <125aryaaman@gmail.com>
2024-04-20 02:49:33 +0530

    Update icon class names

--- components/ui/Icons/Email.tsx
+++ components/ui/Icons/Email.tsx
@@ -10,7 +10,7 @@ const IconEmail = () => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="feather feather-mail"
+    className="feather"
   >
     <title>Email</title>
     <path d="M21 5H3C1.9 5 1.01 5.9 1.01 7L0.99 17C0.99 18.1 1.9 19 3 19H21C22.1 19 23 18.1 23 17V7C23 5.9 22.1 5 21 5Z" />
--- components/ui/Icons/Instagram.tsx
+++ components/ui/Icons/Instagram.tsx
@@ -10,7 +10,7 @@ const IconInstagram = () => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="feather feather-instagram">
+    className="feather">
     <title>Instagram</title>
     <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
     <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
