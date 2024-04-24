715536a241dcdc5c5a64ccaea51f9d50a53b9114
Aman Arya <125aryaaman@gmail.com>
2024-04-24 22:51:26 +0530

    Refactor WebGLWrapper component to use useDelayedMount hook instead of useIsClient

--- components/wrapper/WebGLWrapper/WebGLWrapper.tsx
+++ components/wrapper/WebGLWrapper/WebGLWrapper.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { useIsClient, useWebGLDetection } from '@/hooks';
+import { useDelayedMount, useWebGLDetection } from '@/hooks';
 import { ReadOnlyChildren } from '@/utils/types';
 
 const WebGLWrapper: React.FC<ReadOnlyChildren> = ({ children }) => {
   const { isSupported, error } = useWebGLDetection();
-  const isClient = useIsClient();
+  const isMounted = useDelayedMount(100);
 
   // In client only and Check if the browser supports WebGL
-  if (!isClient || !isSupported) {
+  if (!isMounted || !isSupported) {
     // Todo: add a toast notification, use error
     return null;
   }
@@ -15,6 +15,6 @@ const WebGLWrapper: React.FC<ReadOnlyChildren> = ({ children }) => {
   // Three.js functionality is not supported on your browser
   // Background graphics will not be displayed
   return children;
-};
+}
 
 export default WebGLWrapper;
