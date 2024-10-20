349dd5adee60ca5853ba57445a604f7b5364030c
Aman Arya <125aryaaman@gmail.com>
2024-10-20 07:44:58 +0530

    Refactor NavIcons to use code in mobile view

--- components/ui/NavIcons/NavIcons.tsx
+++ components/ui/NavIcons/NavIcons.tsx
@@ -5,10 +5,11 @@ import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
 import VisuallyHidden from '@/components/wrapper/VisuallyHidden/VisuallyHidden';
 import { Icon } from '@/components/ui/Icons';
-import { useDelayedMount } from '@/hooks';
+import { useDelayedMount, useMediaQuery } from '@/hooks';
 import { socialMediaDetails } from '@/utils/config';
-import styles from './NavIcons.module.css';
 import { cn } from '@/utils/cn';
+import { Code } from '@nextui-org/react';
+import styles from './NavIcons.module.css';
 
 // in milliseconds
 const socialSideBarMountDelay = 500 + 1000;
@@ -23,16 +24,31 @@ const fadeInAnimationVariants = {
 
 export default function SocialNavIcons() {
   const isMounted = useDelayedMount(socialSideBarMountDelay);
+  const isMobile = useMediaQuery('(max-width: 767.5px)');
 
   return (
     <AnimatePresence>
-      {isMounted ? <AnimatedNavIcons /> : <HiddenNavIcons />}
+      {
+        !isMounted ? <HiddenNavIcons /> :
+          isMobile ? <AnimatedNavIconsMobile /> :
+            <AnimatedNavIconsDesktop />
+      }
     </AnimatePresence>
   );
 }
 
-const AnimatedNavIcons = () => (
-  <ul className={styles.StyledSocialList}>
+const AnimatedNavIconsMobile = () => (
+  <Code className={styles.listPosition}>
+    <AnimatedNavIcons />
+  </Code>
+);
+
+const AnimatedNavIconsDesktop = () => (
+  <AnimatedNavIcons className={styles.listPosition} />
+);
+
+const AnimatedNavIcons: React.FC<{ className?: string }> = ({ className }) => (
+  <ul className={cn(className, styles.list)}>
     {socialMediaDetails && socialMediaDetails.map(({ url, name }, index) => (
       <motion.li
         key={index}
@@ -62,15 +78,3 @@ const HiddenNavIcons = () => (
     </ul>
   </VisuallyHidden>
 );
-
-export const NavIcons: React.FC<{ className?: string }> = ({ className }) => (
-  <ul className={cn(className, styles.StyledSocialList)}>
-    {socialMediaDetails && socialMediaDetails.map(({ url, name }, index) => (
-      <li key={index}>
-        <Link href={url} aria-label={name} target="_blank" rel="noreferrer">
-          <Icon name={name} />
-        </Link>
-      </li>
-    ))}
-  </ul>
-)
