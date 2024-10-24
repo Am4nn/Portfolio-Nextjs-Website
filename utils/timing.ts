ba53b7a4cdd1e8272e7f982300352eb3e193c4b1
Aman Arya <125aryaaman@gmail.com>
2024-10-25 04:01:48 +0530

    Add timing constants for various components

---
+++ utils/timing.ts
@@ -0,0 +1,21 @@
+import { MilliSeconds, Seconds } from "@/utils/types";
+
+export const NAVBAR_LOAD_DELAY: Seconds = 0.35;
+export const NAVBAR_LOAD_DURATION: Seconds = 0.3;
+
+export const LOGO_LOAD_DELAY: Seconds = 0.5;
+export const LOGO_LOAD_DURATION: Seconds = 0.3;
+
+export const NAV_ICONS_LOAD_DELAY: Seconds = 0.5 + 1;
+export const NAV_ICONS_LOAD_DURATION: Seconds = 0.3;
+
+export const DISPLACEMENT_SPHERE_LOAD_DURATION: Seconds = 2;
+
+export const INTRO_LOAD_DELAY: Seconds = 1;
+export const INTRO_LOAD_DURATION: Seconds = 0.3;
+
+export const DECODER_TEXT_START_DELAY: MilliSeconds = 1000 + 100;
+
+export const SCRAMBLING_TEXT_DELAY: MilliSeconds = 1500;
+
+export const SCROLL_DOWN_LOAD_DELAY: MilliSeconds = 2000;
