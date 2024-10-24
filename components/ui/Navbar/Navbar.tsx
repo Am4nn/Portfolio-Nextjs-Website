793d6ee04d747a9bd3c33e1d257c60659708e69c
Aman Arya <125aryaaman@gmail.com>
2024-10-25 04:04:53 +0530

    Refactor Navbar component to use framer-motion for animation and timing constants for load delay and duration

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -3,21 +3,29 @@
 import React from "react";
 import {
   Navbar as NextUINavbar, NavbarBrand, Button, Link,
-  NavbarMenuToggle, NavbarContent, NavbarItem
+  NavbarContent, NavbarItem
 } from "@nextui-org/react";
 import Logo from "@/components/ui/Logo/Logo";
 import ThemeSwitch from "@/components/ui/ThemeSwitch/ThemeSwitch";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { NAVBAR_LOAD_DELAY, NAVBAR_LOAD_DURATION } from "@/utils/timing";
 
-const Navbar: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+const navbarVaraints: Variants = {
+  initial: { opacity: 0, filter: "blur(20px)" },
+  blurIn: { opacity: 1, filter: "blur(0px)", transition: { duration: NAVBAR_LOAD_DURATION, delay: NAVBAR_LOAD_DELAY, ease: 'easeInOut' } }
+};
 
-  return (
+const Navbar: React.FC = () => (
+  <AnimatePresence>
     <NextUINavbar
-      isMenuOpen={isMenuOpen}
-      onMenuOpenChange={setIsMenuOpen}
+      as={motion.nav}
       isBlurred={false}
       maxWidth="full"
-      className={`${isMenuOpen ? "bg-background" : "bg-transparent"} fixed transition-colors-400`}
+      className="opacity-0 bg-transparent fixed transition-colors-400"
+      initial="initial"
+      animate="blurIn"
+      exit="initial"
+      variants={navbarVaraints}
     >
       <NavbarBrand>
         <Logo />
@@ -25,22 +33,36 @@ const Navbar: React.FC = () => {
 
       <NavbarContent className="transition-colors-400" justify="end">
         <NavbarItem>
-          <Button as={Link} color="primary" href="mailto:125aryaaman@gmail.com" aria-label="Contact Me" variant="solid" radius="sm">
+          <Button
+            as={Link}
+            color="primary"
+            href="mailto:125aryaaman@gmail.com"
+            aria-label="Contact Me"
+            variant="solid"
+            radius="sm"
+          >
             Contact
           </Button>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="default" target="_blank" href="/resume.pdf" aria-label="Resume" variant="flat" radius="sm">
+          <Button
+            as={Link}
+            color="default"
+            target="_blank"
+            href="/resume.pdf"
+            aria-label="Resume"
+            variant="flat"
+            radius="sm"
+          >
             Resume
           </Button>
         </NavbarItem>
         <NavbarItem>
           <ThemeSwitch />
         </NavbarItem>
-        {/* <NavbarMenuToggle className="md:hidden" aria-label={isMenuOpen ? "Close menu" : "Open menu"} /> */}
       </NavbarContent>
     </NextUINavbar >
-  );
-}
+  </AnimatePresence>
+);
 
 export default Navbar;
